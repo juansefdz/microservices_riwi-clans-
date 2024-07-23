1a5b76b4241e3d565023d5534bc80a875c5005fc
juansefdz <jsfm81091@gmail.com>
2024-07-23 12:28:12 -0500

    update

--- clanes-crud/src/main/java/com/riwi/clanes_crud/services/ClanService.java
+++ clanes-crud/src/main/java/com/riwi/clanes_crud/services/ClanService.java
@@ -1,7 +1,7 @@
 package com.riwi.clanes_crud.services;
 
-import com.riwi.clanes_crud.entities.Cohort;
-import com.riwi.clanes_crud.repositories.CohortRepository;
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,13 +11,13 @@ import com.riwi.clanes_crud.dto.request.ClanGetReq;
 import com.riwi.clanes_crud.dto.request.ClanReq;
 import com.riwi.clanes_crud.dto.request.ClanUpdateReq;
 import com.riwi.clanes_crud.entities.Clan;
+import com.riwi.clanes_crud.entities.Cohort;
 import com.riwi.clanes_crud.repositories.ClanRepository;
+import com.riwi.clanes_crud.repositories.CohortRepository;
 import com.riwi.clanes_crud.services.abtract_service.IClanService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
@@ -46,24 +46,30 @@ public class ClanService implements IClanService {
 
     @Override
     public Clan create(ClanReq req) {
-        Cohort cohort=this.cohortRepository.findById(req.getCohortId()).orElseThrow(()->new RuntimeException("cohort not found"));
+       Cohort cohort = this.cohortRepository.findById(req.getCohortId())
+            .orElseThrow(() -> new RuntimeException("Cohort not found"));
+        
         Clan clan = Clan.builder()
-                .name(req.getName())
-                .description(req.getDescription())
-                .cohort(cohort)
-                .build();
+            .name(req.getName())
+            .description(req.getDescription())
+            .cohort(cohort)
+            .build();
+
         return this.clanRepository.save(clan);
     }
 
     @Override
     public Clan update(Long id, ClanUpdateReq req) {
-        Clan clan = this.clanRepository.findById(id). orElseThrow(()->new RuntimeException("clan not found"));
+       Clan clan  = this.clanRepository.findById(id)
+        .orElseThrow( () -> new RuntimeException("Clan not found"));
 
-        if (req.getCohortId() != clan.getCohort().getId()) {
-            Cohort cohort = this.cohortRepository.findById(id).orElseThrow(()->new RuntimeException("cohort not found"));
+        if(req.getCohortId() != clan.getCohort().getId()){
+            Cohort cohort = this.cohortRepository.findById(req.getCohortId())
+                .orElseThrow(() -> new RuntimeException("Cohort not found"));
+
             clan.setCohort(cohort);
-
         }
+
         clan.setName(req.getName());
         clan.setDescription(req.getDescription());
         clan.setUpdatedAt(LocalDateTime.now());
@@ -73,9 +79,12 @@ public class ClanService implements IClanService {
 
     @Override
     public Clan disable(Long id) {
-        Clan clan = this.clanRepository.findById(id). orElseThrow(()->new RuntimeException("cohort not found"));
+        Clan clan  = this.clanRepository.findById(id)
+        .orElseThrow( () -> new RuntimeException("Clan not found"));
+
         clan.setIsActive(false);
         clan.setUpdatedAt(LocalDateTime.now());
+
         return this.clanRepository.save(clan);
     }
     
